595c428d0e9c0ed0b8cbf01a320aefd3e262178a
eunji <dmswl0489@gmail.com>
2024-11-30 11:38:03 +0900

    init: 프로젝트 초기설정

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from "vue";
+
+const radioTest = ref(false);
+</script>
+
+<template>
+  <q-card>
+      <p>메이플 시뮬레이터</p>
+    <q-btn outline flat>테스트</q-btn>
+    <q-radio v-model="radioTest" val="Y" label="테스트 라디오" />
+  </q-card>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { Quasar, QBtn, QRadio } from 'quasar';
+import 'quasar/src/css/index.sass';
+import '@quasar/extras/material-icons/material-icons.css';
+
+const app = createApp(App);
+
+app.use(Quasar, {
+    components: { QBtn, QRadio },
+    config: {}
+});
+
+app.mount('#app');
